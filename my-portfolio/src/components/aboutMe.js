77a4431769f1a591b525663ea1b133bec6476c3d
Bartok <thom.huenger@gmail.com>
2021-05-03 23:38:42 -0400

    setting up components

---
+++ my-portfolio/src/components/aboutMe.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "../App.css";
+import "bootstrap/dist/css/boostrap.min.css";
+
+function aboutMe(props) {
+    <div className="aboutMe">
+
+        <div className="container">
+            <div className="card">
+                <div className="card-header">
+                    <h3>
+                        About Me
+                    </h3>
+                </div>
+                <div className="card-body">
+                    <blockquote className="blockquote mb-0">
+                        <div className="row">
+                            <div className="col-md-3">
+                                <img src="assets\rsz_10.png" class="hs" />
+
+                                    <p>
+                                        Welcome to my personal portfolio! Here you can view some of my recent development projects
+                                        download my resume, or send me an email, but first let me introduce myself.
+                                        My name is Thom Huenger and I am a musician and teaching artist transitioning into web development.
+                                        I am finishing a full-stack bootcamp through Johns Hopkins University in May 2021.
+                                        I am seeking any and all opportunities to continue honing my coding skills after I complete the coding bootcamp.
+                                        My background is in music composition, theatre, and education which has giving me important experience in communication and
+                                        collaboration, which I have learned to be an intergral component to web development.
+                                    </p>
+
+                                    <p>
+                                        In addition to my skill set as a musician, I can juggle poorly, and make pretty good pizza. When I grow up I
+                                        want to be a internationally recognized pop music icon with a successful crossover career
+                                        in film/television, and an impressive, and diverse, investment portfolio.
+          <h4>Download Resume <a href="assets\ThomHuengerResume.pdf" download>Here</a></h4>
+                                        <h4>Visit my <a href="https://www.linkedin.com/in/thom-huenger-b81893158/">LinkedIn</a> </h4>
+                                    </p>
+
+</div>
+                        </div>
+</blockquote>
+                </div>
+
+            </div>
+    </div>  
+    </div>      
+    
+}
+export default aboutMe;
